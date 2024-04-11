6de4ed25c4efec3a22218b59e68ee6dfb24f84cf
kusum prasad <s.kusi04@gmail.com>
2024-04-11 21:25:20 +1200

    This is my initial commit

---
+++ src/atbx6firsttestcase/HelloWorld.java
@@ -0,0 +1,27 @@
+package atbx6firsttestcase;
+
+//class ->Method ->Statements
+
+
+//class
+public class HelloWorld {
+
+	
+	//Method
+	public static void main(String[] args) {
+		System.out.println("Hello World!!\n");
+
+		System.out.println("Hello World\t");
+		
+		System.out.println(2+2);
+		//variable is container which hold data
+		int age = 65;
+		age = 70;
+		
+		
+		
+		age = age + 1;
+		System.out.println(age);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module atbx6firsttestcase {
+}
