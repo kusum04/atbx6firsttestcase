dd0ee2dbe50768611fbd8b0c3801574ed52bf658
kusum <s.kusi04@gmail.com>
2024-04-11 21:52:37 +1200

    add calculator class

---
+++ src/atbx6firsttestcase/math/Calculator.java
@@ -0,0 +1,12 @@
+package atbx6firsttestcase.math;
+
+public class Calculator {
+	public static void main(String args[]) {
+		int a = 3;
+		int b = 6;
+		System.out.println("sum: "+ (a+b));
+		System.out.println("sub: "+ (a-b));
+		System.out.println("mul: "+ (a*b));
+		System.out.println("dev: "+ (a/b));
+	}
+}
